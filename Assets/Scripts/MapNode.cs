6e6db024bba59cc78ff7379e44b55bfe68e07ad1
Vladimir Limarchenko <perspective_games@yahoo.com>
2020-02-10 05:55:40 +0200

    Created starter scripts for node SO and node object

---
+++ Assets/Scripts/MapNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public List<MapNode> incomingConnections;
+    public List<MapNode> outgoingConnections;
+
+    public NodeBlueprint Blueprint { get; private set; }
+
+    public void ApplyBlueprint(NodeBlueprint blueprint)
+    {
+        Blueprint = blueprint;
+        sr.sprite = blueprint.sprite;
+    }
+}
---
+++ Assets/Scripts/NodeBlueprint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public enum NodeType
+{
+    MinorEnemy, 
+    RestSite, 
+    Treasure, 
+    Store, 
+    Boss, 
+    Mystery
+}
+
+[CreateAssetMenu]
+public class NodeBlueprint : ScriptableObject
+{
+    public Sprite sprite;
+    public NodeType nodeType;
+
+    public void EnterNode()
+    {
+        switch (nodeType)
+        {
+            case NodeType.MinorEnemy:
+                break;
+            case NodeType.RestSite:
+                break;
+            case NodeType.Treasure:
+                break;
+            case NodeType.Store:
+                break;
+            case NodeType.Boss:
+                break;
+            case NodeType.Mystery:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
