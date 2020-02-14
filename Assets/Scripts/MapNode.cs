d50885adcf8c2c897566abd079c94422a78514e6
Vladimir Limarchenko <perspective_games@yahoo.com>
2020-02-15 00:55:12 +0200

    Replaced ravenmore icons

--- Assets/Scripts/MapNode.cs
+++ Assets/Scripts/MapNode.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class MapNode : MonoBehaviour
 {
     public SpriteRenderer sr;
+    public SpriteRenderer visitedCircle;
     public Color32 attainableColor = Color.white;
     public Color32 lockedColor = Color.gray;
     public readonly List<MapNode> IncomingConnections = new List<MapNode>();
@@ -12,11 +14,17 @@ public class MapNode : MonoBehaviour
     public NodeBlueprint Blueprint { get; private set; }
     public int LayerIndex { get; private set; }
 
+    private float initialScale;
+    private const float HoverScaleFactor = 1.2f;
+    
     public void SetUp(NodeBlueprint blueprint, int layerIndex)
     {
         Blueprint = blueprint;
         LayerIndex = layerIndex;
         sr.sprite = blueprint.sprite;
+        initialScale = sr.transform.localScale.x;
+        visitedCircle.color = attainableColor;
+        visitedCircle.gameObject.SetActive(false);
     }
 
     public void AddIncoming(MapNode node)
@@ -55,5 +63,17 @@ public class MapNode : MonoBehaviour
     public void SetAttainable(bool attainable)
     {
         sr.color = attainable ? attainableColor : lockedColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        sr.transform.DOKill();
+        sr.transform.DOScale(initialScale * HoverScaleFactor, 0.3f);
+    }
+    
+    private void OnMouseExit()
+    {
+        sr.transform.DOKill();
+        sr.transform.DOScale(initialScale, 0.3f);
     }
 }
---
+++ Assets/Scripts/MapPlayerTracker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPlayerTracker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
