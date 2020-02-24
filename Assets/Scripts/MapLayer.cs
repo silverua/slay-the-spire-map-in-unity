624e3e53786f11e91fdbe384214ac70dc49ec6dc
Vladimir Limarchenko <perspective_games@yahoo.com>
2020-02-24 19:02:06 +0200

    Tooltips in MapLayer.cs

--- Assets/Scripts/MapLayer.cs
+++ Assets/Scripts/MapLayer.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 [System.Serializable]
 public class MapLayer
 {
+    [Tooltip("Default node for this map layer. If Randomize Nodes is 0, you will get this node 100% of the time")]
     public NodeBlueprint node;
     // [OneLineWithHeader] public IntMinMax numOfNodes;
     [OneLineWithHeader] public FloatMinMax distanceFromPreviousLayer;
+    [Tooltip("Distance between the nodes on this layer")]
     public float nodesApartDistance;
+    [Tooltip("If this is set to 0, nodes on this layer will appear in a straight line. Closer to 1f = more position randomization")]
     [Range(0f, 1f)] public float randomizePosition;
+    [Tooltip("Chance to get a random node that is different from the default node on this layer")]
     [Range(0f, 1f)] public float randomizeNodes;
 }
